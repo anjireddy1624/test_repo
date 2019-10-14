978a6ace67153f06452e251d2da965ac8e0ecde1
anjireddy1624 <yagnamanji424@gmail.com>
2019-10-14 16:50:38 +0530

    secondfile

---
+++ Maven.java
@@ -0,0 +1,16 @@
+class Maven
+{
+	public static void main(String[] args) 
+	{
+		int n=5;
+		for (int i=0;i<n;i++)
+		{
+			for (int j=i+0;j<5;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+
+		}	
+	}
+}
