b435da2d1b386883d15a2bf7ac2085fd835ad1d9
anjireddy1624 <yagnamanji424@gmail.com>
2019-10-16 09:52:46 +0000

    two

---
+++ Two.java
@@ -0,0 +1,16 @@
+class Two 
+{
+	public static void main(String [] ar)
+	{
+		int n=5;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+0;j<n;j++)
+			{
+			System.out.println("*");
+			}
+		System.out.println( );
+		}
+	}
+}
+
